689ff97bd6918867b84b46608b0b2963a8abfe67
Hans Hübner <hans.huebner@gmail.com>
2021-06-12 10:59:33 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const SerialPort = require('serialport');
+const app = express();
+const port = 3000;
+
+const serialport = new SerialPort('COM10');
+const Readline = SerialPort.parsers.Readline
+const parser = serialport.pipe(new Readline({delimiter: '\r'}));
+
+let status = false;
+
+app.get('/', (req, res) => {
+  status = !status;
+  const message = 'DIG1 ' + (status ? '1' : '0');
+  parser.once('data', response => {
+    res.send('Response: ' + JSON.stringify(response));
+    console.log(message, '=>', response);
+  });
+  serialport.write(message + '\r');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
